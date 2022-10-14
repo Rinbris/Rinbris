d4c58d1a85645b53d43bc740d71ec5f93b59a886
Lamboley <lucaslamboley@outlook.com>
2022-10-14 22:01:29 +0200

    Add command to set current

--- Rinbris/Core/Commands.lua
+++ Rinbris/Core/Commands.lua
@@ -27,6 +27,17 @@ function E:SetPetID(petID)
     end
 end
 
+function E:SetCurrentPetID()
+    local summonedPetGUID = C_PetJournalGetSummonedPetGUID()
+    if summonedPetGUID then
+        local name = select(8, C_PetJournalGetPetInfoByPetID(summonedPetGUID))
+        if name then
+            RinbrisCharacterDB.battlePet = summonedPetGUID
+            self.Print(name .. ' configured.')
+        end
+    end
+end
+
 function E:GetPetID()
     local summonedPetGUID = C_PetJournalGetSummonedPetGUID()
     if summonedPetGUID then
@@ -65,6 +76,7 @@ end
 
 function E:LoadCommands()
     self:RegisterChatCommand('rinbrissetpetid', 'SetPetID')
+    self:RegisterChatCommand('rinbrissetcurrentpetid', 'SetCurrentPetID')
     self:RegisterChatCommand('rinbrisgetpetid', 'GetPetID')
     self:RegisterChatCommand('rinbrischeckpetid', 'CheckPetID')
     self:RegisterChatCommand('rinbrismountid', 'GetMountID')
