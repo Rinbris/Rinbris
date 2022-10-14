7486c3530579003145eaeece87806c04a5140949
Lamboley <lucaslamboley@outlook.com>
2022-10-14 22:48:50 +0200

    Maybe remove

--- Rinbris/Core/Commands.lua
+++ Rinbris/Core/Commands.lua
@@ -7,30 +7,29 @@ local select = select
 local pairs = pairs
 
 -- WoW APIs
-local C_PetJournalGetSummonedPetGUID = C_PetJournal.GetSummonedPetGUID
-local C_PetJournalGetPetInfoByPetID = C_PetJournal.GetPetInfoByPetID
-local C_MountJournalGetMountInfoByID = C_MountJournal.GetMountInfoByID
-local C_MountJournalGetMountIDs = C_MountJournal.GetMountIDs
+local C_PetJournal_GetSummonedPetGUID = C_PetJournal.GetSummonedPetGUID
+local C_PetJournal_GetPetInfoByPetID = C_PetJournal.GetPetInfoByPetID
+-- local C_MountJournal_GetMountInfoByID = C_MountJournal.GetMountInfoByID
 
-local C_MapGetBestMapForUnit = C_Map.GetBestMapForUnit
-local C_MapGetMapInfo = C_Map.GetMapInfo
+-- local C_Map_GetBestMapForUnit = C_Map.GetBestMapForUnit
+-- local C_Map_GetMapInfo = C_Map.GetMapInfo
 
-function E:SetPetID(petID)
-    if petID == nil then
-        return
-    end
+-- function E:SetPetID(petID) -- TODO: Is it still usefull ?
+--     if petID == nil then
+--         return
+--     end
 
-    local name = select(8, C_PetJournalGetPetInfoByPetID(petID))
-    if name then
-        RinbrisCharacterDB.battlePet = petID
-        self.Print(name .. ' configured.')
-    end
-end
+--     local name = select(8, C_PetJournal_GetPetInfoByPetID(petID))
+--     if name then
+--         RinbrisCharacterDB.battlePet = petID
+--         self.Print(name .. ' configured.')
+--     end
+-- end
 
 function E:SetCurrentPetID()
-    local summonedPetGUID = C_PetJournalGetSummonedPetGUID()
+    local summonedPetGUID = C_PetJournal_GetSummonedPetGUID()
     if summonedPetGUID then
-        local name = select(8, C_PetJournalGetPetInfoByPetID(summonedPetGUID))
+        local name = select(8, C_PetJournal_GetPetInfoByPetID(summonedPetGUID))
         if name then
             RinbrisCharacterDB.battlePet = summonedPetGUID
             self.Print(name .. ' configured.')
@@ -39,7 +38,7 @@ function E:SetCurrentPetID()
 end
 
 function E:GetPetID()
-    local summonedPetGUID = C_PetJournalGetSummonedPetGUID()
+    local summonedPetGUID = C_PetJournal_GetSummonedPetGUID()
     if summonedPetGUID then
         self.Print('Current petID is : ' .. summonedPetGUID)
     end
@@ -48,37 +47,37 @@ end
 function E:CheckPetID()
     local characterBattlePet = RinbrisCharacterDB.battlePet
     if characterBattlePet then
-        self.Print(select(8, C_PetJournalGetPetInfoByPetID(characterBattlePet)) .. ' configured on a character level.')
+        self.Print(select(8, C_PetJournal_GetPetInfoByPetID(characterBattlePet)) .. ' configured on a character level.')
     else 
-        self.Print(select(8, C_PetJournalGetPetInfoByPetID(M.db.companion)) .. ' configured on a profile level.')
+        self.Print(select(8, C_PetJournal_GetPetInfoByPetID(M.db.companion)) .. ' configured on a profile level.')
     end
 end
 
-function E:ChatCommand_GetMountID(spellID)
-    if spellID == nil then
-        return
-    end
+-- function E:ChatCommand_GetMountID(spellID)
+--     if spellID == nil then
+--         return
+--     end
 
-    local mountIDs = C_MountJournalGetMountIDs
-    for _, mountID in pairs(mountIDs) do
-        local mountName, mountSpellID = C_MountJournalGetMountInfoByID(mountID)
+--     local mountIDs = C_MountJournalGetMountIDs
+--     for _, mountID in pairs(mountIDs) do
+--         local mountName, mountSpellID = C_MountJournal_GetMountInfoByID(mountID)
 
-        if mountSpellID == tonumber(spellID) then
-            self.Print(strjoin(' ', mountName, mountID))
-        end
-    end
-end
+--         if mountSpellID == tonumber(spellID) then
+--             self.Print(strjoin(' ', mountName, mountID))
+--         end
+--     end
+-- end
 
-function E:ChatCommand_ShowZoneID()
-    local id = C_MapGetBestMapForUnit('player')
-    self.Print(strjoin(' - ', id, C_MapGetMapInfo(id).name))
-end
+-- function E:ChatCommand_ShowZoneID()
+--     local id = C_Map_GetBestMapForUnit('player')
+--     self.Print(strjoin(' - ', id, C_Map_GetMapInfo(id).name))
+-- end
 
 function E:LoadCommands()
-    self:RegisterChatCommand('rinbrissetpetid', 'SetPetID')
-    self:RegisterChatCommand('rinbrissetcurrentpetid', 'SetCurrentPetID')
+    -- self:RegisterChatCommand('rinbrissetpetid', 'SetPetID')
+    self:RegisterChatCommand('rinbrissetpetid', 'SetCurrentPetID')
     self:RegisterChatCommand('rinbrisgetpetid', 'GetPetID')
     self:RegisterChatCommand('rinbrischeckpetid', 'CheckPetID')
-    self:RegisterChatCommand('rinbrismountid', 'GetMountID')
-    self:RegisterChatCommand('rinbriszoneid', 'ChatCommand_ShowZoneID')
+    -- self:RegisterChatCommand('rinbrismountid', 'GetMountID')
+    -- self:RegisterChatCommand('rinbriszoneid', 'ChatCommand_ShowZoneID')
 end
