b9f87e901f30649f3bdff998fdc4c3f4fe077d89
Lamboley <lucaslamboley@outlook.com>
2022-11-15 17:09:26 +0100

    Don't summon if not per char

--- Rinbris/Settings/Profile.lua
+++ Rinbris/Settings/Profile.lua
@@ -1,11 +1,10 @@
 local _, _, _, P = unpack(select(2, ...))
 
 P.misc = {
-    companion = 'BattlePet-0-00000F8DBF00',
 }
 
 P.blizzard = {
-    ObjectiveTrackerScale = 0.84,
+    ObjectiveTrackerScale = 0.85,
 }
 
 P.chat = {
