f16f2cf2d20fc8833ea7d64fe48835694fccbbd7
Lamboley <lucaslamboley@outlook.com>
2022-11-15 17:10:06 +0100

    Hide statusbar

---
+++ Rinbris/Modules/Blizzard/StatusTrackingBar.lua
@@ -0,0 +1,7 @@
+local E = unpack(Rinbris)
+local B = E:GetModule('Blizzard')
+
+function B:StatusTrackingBar()
+    StatusTrackingBarManager:UnregisterAllEvents()
+    StatusTrackingBarManager:Hide()
+end
